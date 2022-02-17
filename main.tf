cb1f91e392076f777c2fda49e8558fd102a63cb4
Manoj Mup <mano@Manojs-MacBook-Pro.local>
2022-02-17 00:36:44 -0600

    Testing

--- main.tf
+++ main.tf
@@ -1,28 +1,41 @@
 provider "aws" {
   region = var.region
+  
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+data "aws_ssoadmin_instances" "example" {}
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+resource "aws_ssoadmin_permission_set" "example" {
+  name             = "Example"
+  description      = "An example"
+  instance_arn     = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  session_duration = "PT2H"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
 
-  tags = {
-    Name = var.instance_name
-  }
-}
+#data "aws_ami" "ubuntu" {
+#  most_recent = true
+#
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#
+#  owners = ["099720109477"] # Canonical
+#}
+#
+#resource "aws_instance" "ubuntu" {
+#  ami           = data.aws_ami.ubuntu.id
+#  instance_type = var.instance_type
+#
+#  tags = {
+#    Name = var.instance_name
+#  }
+#}
+#
